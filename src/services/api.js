a463d93b71b9f74bcf56ba73e6390234aa4bd0fc
Mariam Blanco <mariamblanko@yahoo.es>
2022-06-03 16:27:58 +0200

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 const getAllJobs = () => {
   return fetch(
-    "https://raw.githubusercontent.com/mariam-blanco/server-02/main/db.json"
+    "https://raw.githubusercontent.com/mariam-blanco/server-github-jobs/main/db.json"
   )
     .then((response) => {
       return response.json();
